5ac36a8a9790a205e43d41ba15daaaa168dfc072
abelabelabel <szaboabel2006@gmail.com>
2024-05-20 21:29:13 +0200

    correction

--- source.py
+++ source.py
@@ -1,6 +1,7 @@
 from PIL import Image
+import os
 
-image = Image.open("img.png")
+image = Image.open(f"{os.path.dirname(__file__)}\\img.png")
 
 pixels = image.load()
 
@@ -24,17 +25,27 @@ for y in range(height):
 
         brightness = 0.299 * r + 0.587 * g + 0.114 * b
 
-        if (brightness <= 50):
+        if (brightness <= 25):
             txt += "&"
-        elif (brightness > 50 and brightness <= 100):
+        elif (brightness > 25 and brightness <= 50):
             txt += "\\"
-        elif (brightness > 100 and brightness <=150):
+        elif (brightness > 50 and brightness <= 75):
             txt += "?"
-        elif (brightness > 150 and brightness <= 200):
+        elif (brightness > 75 and brightness <= 100):
             txt += "["
-        elif (brightness > 200 and brightness <= 255):
+        elif (brightness > 100 and brightness <= 125):
             txt += "="
+        elif (brightness > 125 and brightness <= 150):
+            txt += "$"
+        elif (brightness > 150 and brightness <= 175):
+            txt += "*"
+        elif (brightness > 175 and brightness <= 200):
+            txt += "<"
+        elif (brightness > 200 and brightness <= 225):
+            txt += "@"
+        elif (brightness > 225 and brightness <= 255):
+            txt += "+"
     txt += "\n"
 
-with open("ascii.txt", mode='w') as f:
+with open(f"{os.path.dirname(__file__)}\\ascii.txt", mode='w') as f:
     f.write(txt)
